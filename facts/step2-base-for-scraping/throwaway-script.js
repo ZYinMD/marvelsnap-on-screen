9dfcb15dc0b6aa309e357984bcb56c5ac0b07a9f
Z Yin <Z.Yin.MD@gmail.com>
2023-07-11 18:32:51 -0400

    facts: puppeteer outputs json

--- facts/step2-base-for-scraping/throwaway-script.js
+++ facts/step2-base-for-scraping/throwaway-script.js
@@ -12,10 +12,11 @@ const allCards = document.querySelectorAll('a.simple-card');
 const result = [];
 allCards.forEach((i) => {
   const name = i.querySelector('.cardname')?.textContent;
-  const desc = i.querySelector('.card-description')?.textContent?.trim();
+  const descRaw = i.querySelector('.card-description')?.textContent?.trim();
+  const desc = descRaw?.replaceAll(/\s+/g, ' ');
   const imgTag = i.querySelector('img.active');
   const imgFilename = parseImgFilename(imgTag?.src);
-  const cardId = parseCardId(imgFilename);
-  result.push({ name, desc, cardId });
+  const id = parseCardId(imgFilename);
+  result.push({ id, name, desc });
 });
 console.log('result:', result);
--- facts/step3-puppeteer-to-json/get-all-cards.js
+++ facts/step3-puppeteer-to-json/get-all-cards.js
@@ -1,7 +1,37 @@
 import puppeteer from 'puppeteer-core';
+import fs from 'fs';
 const currentModulePath = new URL(import.meta.url).href;
 const browser = await puppeteer.launch({ channel: 'chrome', headless: false });
 const page = await browser.newPage();
 await page.goto(currentModulePath + '/../../step2-base-for-scraping/pasted-div-of-card-list.html', {
   waitUntil: ['load', 'domcontentloaded', 'networkidle0'],
 });
+
+const result = await page.evaluate(() => {
+  function parseImgFilename(imgUrl) {
+    if (!imgUrl) return null;
+    const imgFilename = imgUrl.split('/').at(-1);
+    return imgFilename;
+  }
+  function parseCardId(imgFilename) {
+    if (!imgFilename) return null;
+    const cardId = imgFilename.split('.')[0];
+    return cardId;
+  }
+  const allCards = document.querySelectorAll('a.simple-card');
+  const result = [];
+  allCards.forEach((i) => {
+    const name = i.querySelector('.cardname')?.textContent;
+    const descRaw = i.querySelector('.card-description')?.textContent?.trim();
+    const desc = descRaw?.replaceAll(/\s+/g, ' ');
+    const imgTag = i.querySelector('img.active');
+    const imgFilename = parseImgFilename(imgTag?.src);
+    const id = parseCardId(imgFilename);
+    result.push({ id, name, desc });
+  });
+  return result;
+});
+
+console.log('result:', result);
+fs.writeFileSync('all-cards.json', JSON.stringify(result, null, 2));
+browser.close();
