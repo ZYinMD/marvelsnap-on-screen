4dfba49d117f61b9f1b190f3b3313b2ccf58fe5b
Z Yin <Z.Yin.MD@gmail.com>
2023-07-11 17:43:24 -0400

    facts: throwaway script is goo enough

--- facts/step2-base-for-scraping/throwaway-script.js
+++ facts/step2-base-for-scraping/throwaway-script.js
@@ -1,3 +1,21 @@
-let allCards = Array.from(document.querySelectorAll('a.simple-card'));
-const card1 = allCards[0];
-console.log('card1:', card1);
+function parseImgFilename(imgUrl) {
+  if (!imgUrl) return null;
+  const imgFilename = imgUrl.split('/').at(-1);
+  return imgFilename;
+}
+function parseCardId(imgFilename) {
+  if (!imgFilename) return null;
+  const cardId = imgFilename.split('.')[0];
+  return cardId;
+}
+const allCards = document.querySelectorAll('a.simple-card');
+const result = [];
+allCards.forEach((i) => {
+  const name = i.querySelector('.cardname')?.textContent;
+  const desc = i.querySelector('.card-description')?.textContent?.trim();
+  const imgTag = i.querySelector('img.active');
+  const imgFilename = parseImgFilename(imgTag?.src);
+  const cardId = parseCardId(imgFilename);
+  result.push({ name, desc, cardId });
+});
+console.log('result:', result);
