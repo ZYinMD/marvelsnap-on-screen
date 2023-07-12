e24ea4af0f9fc2e8b1627d50b682d541ae76f31a
Z Yin <Z.Yin.MD@gmail.com>
2023-07-11 23:30:27 -0400

    facts: add "status" to card data

--- facts/step2-base-for-scraping/throwaway-script.js
+++ facts/step2-base-for-scraping/throwaway-script.js
@@ -1,10 +1,11 @@
 const allCards = document.querySelectorAll('a.simple-card');
 const result = [];
 allCards.forEach((i) => {
+  const id = i.getAttribute('href')?.split('/')?.at(-2);
   const name = i.querySelector('.cardname')?.textContent;
   const descRaw = i.querySelector('.card-description')?.textContent?.trim();
   const desc = descRaw?.replaceAll(/\s+/g, ' ');
-  const id = i.getAttribute('href')?.split('/')?.at(-2);
-  result.push({ id, name, desc });
+  const status = i.getAttribute('data-status');
+  result.push({ id, name, desc, status });
 });
 console.info('result:', result);
--- facts/step3-puppeteer-to-json/puppeteer.js
+++ facts/step3-puppeteer-to-json/puppeteer.js
@@ -11,11 +11,12 @@ const result = await page.evaluate(() => {
   const allCards = document.querySelectorAll('a.simple-card');
   const result = [];
   allCards.forEach((i) => {
+    const id = i.getAttribute('href')?.split('/')?.at(-2);
     const name = i.querySelector('.cardname')?.textContent;
     const descRaw = i.querySelector('.card-description')?.textContent?.trim();
     const desc = descRaw?.replaceAll(/\s+/g, ' ');
-    const id = i.getAttribute('href')?.split('/')?.at(-2);
-    result.push({ id, name, desc });
+    const status = i.getAttribute('data-status');
+    result.push({ id, name, desc, status });
   });
   return result;
 });
