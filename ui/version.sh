4f0234dab8a3b048917c4208ca2d01789eeb4b7c
Z Yin <Z.Yin.MD@gmail.com>
2023-08-17 19:41:59 -0400

    ui: add script for git tag

--- ui/version.sh
+++ ui/version.sh
@@ -3,4 +3,6 @@ set -e
 
 pnpm check-all
 git commit -am "$npm_package_name: v$npm_package_version"
-git push # after push, vercel will run "build", so I don't need to build locally
+git tag "v$npm_package_version"
+# git push # after push, vercel will run "build", so I don't need to build locally
+git push --tags
