9a5c6b2d305942aa134f0127ef02013f5c5105f8
Z Yin <Z.Yin.MD@gmail.com>
2023-08-17 19:43:15 -0400

    chore

--- ui/version.sh
+++ ui/version.sh
@@ -4,5 +4,5 @@ set -e
 pnpm check-all
 git commit -am "$npm_package_name: v$npm_package_version"
 git tag "v$npm_package_version"
-# git push # after push, vercel will run "build", so I don't need to build locally
+git push # after push, vercel will run "build", so I don't need to build locally
 git push --tags
