e1e44756fa7a5e760433e88badfeef19eb2efdc7
Z Yin <Z.Yin.MD@gmail.com>
2023-08-17 20:13:12 -0400

    facts: output all movies to ts

--- facts/step3/excel-to-json.js
+++ facts/step3/excel-to-json.js
@@ -7,8 +7,12 @@ const result = {};
 for (const sheetName of workbook.SheetNames) {
   result[sheetName] = utils.sheet_to_json(workbook.Sheets[sheetName]);
 }
+const stringifiedResult = JSON.stringify(
+  result,
+  (key, value) => (key === 'year' ? String(value) : value), // all year should be string not number, because there are some span values like "2019-2020":
+  2,
+);
 
-// all year should be string not number, because there are some span values like "2019-2020":
-const output = JSON.stringify(result, (key, value) => (key === 'year' ? String(value) : value), 2);
-const outputFilePath = new URL('./all-titles.json', import.meta.url);
+const outputFilePath = new URL('../../ui/src/lib/facts/allTitles.ts', import.meta.url);
+const output = `export const allTitles = ${stringifiedResult} as const;`;
 fs.writeFile(outputFilePath, output);
