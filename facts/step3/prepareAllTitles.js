8da94328cec9e7f03884fbe2e1121d015f545ba7
Z Yin <Z.Yin.MD@gmail.com>
2023-08-23 15:08:33 -0400

    facts: step3: switch xlsx import to deno

--- facts/step3/prepareAllTitles.js
+++ facts/step3/prepareAllTitles.js
@@ -1,11 +1,12 @@
-import { read, utils } from 'xlsx/xlsx.mjs';
+import * as XLSX from 'https://unpkg.com/xlsx/xlsx.mjs';
+
 const excelFilePath = new URL('./all-titles.xlsx', import.meta.url);
 const binary = await Deno.readFile(excelFilePath);
-const workbook = read(binary);
+const workbook = XLSX.read(binary);
 const result = {};
 for (const sheetName of workbook.SheetNames) {
   result[sheetName] = {};
-  const rows = utils.sheet_to_json(workbook.Sheets[sheetName]);
+  const rows = XLSX.utils.sheet_to_json(workbook.Sheets[sheetName]);
   rows.forEach((row) => {
     row.year = String(row.year); // all year should be string not number, because there are some span values like "2019-2020":
     const id = `${row.year}|${row.title}`;
