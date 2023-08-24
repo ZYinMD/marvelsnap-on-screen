c9f80a4b0cbffd92046b44dfad7ee02a97d8f80b
Z Yin <Z.Yin.MD@gmail.com>
2023-08-24 17:40:20 -0400

    ui > main list: add all shows, and dividers

--- ui/src/lib/MainList/$mainList.ts
+++ ui/src/lib/MainList/$mainList.ts
@@ -3,6 +3,23 @@ import { allTitles } from '../facts/allTitles';
 
 export const mainList = derived([], () => {
   const result = [];
+  result.push({
+    title: 'Movies',
+    type: 'divider',
+    key: 'heading-movies',
+  });
   result.push(...Object.values(allTitles.movies));
+  result.push({
+    title: 'Live Action TV Series',
+    type: 'divider',
+    key: 'heading-live-action-tv-series',
+  });
+  result.push(...Object.values(allTitles['live-action-tv-series']));
+  result.push({
+    title: 'Animated TV Series',
+    type: 'divider',
+    key: 'heading-animated-tv-series',
+  });
+  result.push(...Object.values(allTitles['animated-tv-series']));
   return result;
 });
