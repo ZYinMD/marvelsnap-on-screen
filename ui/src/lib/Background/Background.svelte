664bc4acd107d05d34fef73df3d086cf743ac700
Z Yin <Z.Yin.MD@gmail.com>
2023-08-29 17:05:35 -0400

    ui: add the "Background" component to include all backgrounds

--- ui/src/lib/Background/Background.svelte
+++ ui/src/lib/Background/Background.svelte
@@ -1,6 +1,10 @@
-<!-- @component the rotating background  -->
+<script lang="ts">
+  import RotatingMix from './RotatingMix.svelte';
+</script>
+
+<!-- @component The animated background of app -->
 <div class="container">
-  <div class="background rotate"></div>
+  <RotatingMix />
 </div>
 
 <style>
@@ -10,35 +14,9 @@
     height: 100vh;
     overflow: hidden;
     display: grid;
-    place-items: center;
-  }
-  .background {
-    --color3: rgb(0, 66, 97);
-    --color2: rgb(26, 0, 58);
-    --color1: rgb(42, 0, 122);
-    --color4: rgb(96, 0, 40);
-    width: max(100vw, 100vh);
-    height: max(100vw, 100vh);
-    transform-origin: center;
-    scale: 1.5;
-    background: linear-gradient(to bottom, var(--color1), var(--color2));
-  }
-
-  .background::after {
-    content: '';
-    position: absolute;
-    width: inherit;
-    height: inherit;
-    background: linear-gradient(to bottom, var(--color3), var(--color4));
-    -webkit-mask-image: linear-gradient(to left, var(--color4), transparent);
-    mask-image: linear-gradient(to left, var(--color4), transparent);
-  }
-  .rotate {
-    animation: rotate 25s linear infinite;
-  }
-  @keyframes rotate {
-    to {
-      transform: rotate(360deg);
-    }
+    grid:
+      'only' auto
+      /
+      auto;
   }
 </style>
---
+++ ui/src/lib/Background/RotatingMix.svelte
@@ -0,0 +1,41 @@
+<!-- @component the rotating background  -->
+<div class="container">
+  <div class="background rotate"></div>
+</div>
+
+<style>
+  .container {
+    grid-area: only;
+    display: grid;
+    place-items: center;
+  }
+  .background {
+    --color3: rgb(0, 66, 97);
+    --color2: rgb(26, 0, 58);
+    --color1: rgb(42, 0, 122);
+    --color4: rgb(96, 0, 40);
+    width: max(100vw, 100vh);
+    height: max(100vw, 100vh);
+    transform-origin: center;
+    scale: 1.5;
+    background: linear-gradient(to bottom, var(--color1), var(--color2));
+  }
+
+  .background::after {
+    content: '';
+    position: absolute;
+    width: inherit;
+    height: inherit;
+    background: linear-gradient(to bottom, var(--color3), var(--color4));
+    -webkit-mask-image: linear-gradient(to left, var(--color4), transparent);
+    mask-image: linear-gradient(to left, var(--color4), transparent);
+  }
+  .rotate {
+    animation: rotate 25s linear infinite;
+  }
+  @keyframes rotate {
+    to {
+      transform: rotate(360deg);
+    }
+  }
+</style>
