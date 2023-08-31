c0aa1350d971562da61a659e3309bddf88dc52dd
Z Yin <Z.Yin.MD@gmail.com>
2023-08-31 16:11:03 -0400

    ui: overhaul div structure of list items

--- ui/src/lib/MainList/Entry/Entry.svelte
+++ ui/src/lib/MainList/Entry/Entry.svelte
@@ -16,54 +16,48 @@
   $: isOpen = $openDrawers.has(key);
 </script>
 
-<!-- @component the clickable movie title (or a divider) -->
-<div class="container">
+<!-- @component one item in the list -->
+<div
+  class={`container ${type}`}
+  on:click={handleClick}
+  tabindex="0"
+  role="button"
+  on:keydown={handleClick}
+>
+  <Chevron {isOpen} />
   {#if type === 'movie'}
-    <Chevron {isOpen} />
-    <div class="movie" on:click={handleClick} tabindex="0" role="button" on:keydown={handleClick}>
-      <MovieRow {year} {title} />
-    </div>
+    <MovieRow {year} {title} />
   {:else}
-    <div
-      class="tv-series"
-      on:click={handleClick}
-      tabindex="0"
-      role="button"
-      on:keydown={handleClick}
-    >
-      <Chevron {isOpen} />
-      <TvText {year} {title} {numSeasons} {numEpisodes} />
-    </div>
+    <TvText {year} {title} {numSeasons} {numEpisodes} />
   {/if}
 </div>
 
 <style>
   .container {
-    --movie-height: 40px;
-    --tv-series-height: 50px;
-    --padding-left: 10px;
     margin: 10px 0;
-    opacity: 0.9;
     cursor: pointer;
-    display: flex;
-    flex-direction: column;
-  }
-  .movie {
-    border: 1px grey solid;
-    height: var(--movie-height);
-    padding-left: var(--padding-left);
     display: grid;
     align-items: center;
-    border: 1px Thistle solid;
-    box-shadow: inset 0 0 3px Thistle;
+    grid:
+      'chevron text' auto
+      /
+      44px auto;
   }
-  .tv-series {
-    height: var(--tv-series-height);
-    padding-left: var(--padding-left);
+  .movie {
+    height: 40px;
     border: 1px Thistle solid;
-    box-shadow:
+    /* box-shadow: inset 0 0 3px Thistle; */
+  }
+  .live-action-tv-series {
+    height: 50px;
+    border: 1px SkyBlue solid;
+    /* box-shadow:
       inset 0 0 5px Thistle,
-      0 0 3px Thistle;
+      0 0 3px Thistle; */
     /* background-image: radial-gradient(#4444, transparent); */
+  }
+  .animated-tv-series {
+    height: 50px;
+    border: 1px PaleGreen solid;
   }
 </style>
