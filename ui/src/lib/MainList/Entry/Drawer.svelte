618012fb6ad5036c503cd446e49f4c312621b913
Z Yin <Z.Yin.MD@gmail.com>
2023-08-31 20:10:39 -0400

    ui: show cards!

--- ui/src/lib/MainList/Entry/Drawer.svelte
+++ ui/src/lib/MainList/Entry/Drawer.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+  import { allCards } from '../../facts/allCards';
   import { map, type Key, type Card } from '../../facts/map';
   export let key: Key;
   const major = map[key].major;
@@ -8,13 +9,23 @@
 <!-- @component the expandable drawer that contains cards -->
 <div class="container">
   {#each major as card (card)}
-    <div>
-      {card}
-    </div>
+    {@const filename = allCards[card].defId}
+    <img src={`/card-images/${filename}.webp`} alt={card} />
   {/each}
 </div>
 
 <style>
   .container {
+    --good-width: min(30vw, 130px);
+    grid-area: drawer;
+    display: grid;
+    grid-template-columns: repeat(auto-fit, var(--good-width));
+    justify-content: center;
+    padding-bottom: 15px;
+  }
+  img {
+    width: var(--good-width);
+    height: calc(var(--good-width) * 1.3);
+    object-fit: cover;
   }
 </style>
