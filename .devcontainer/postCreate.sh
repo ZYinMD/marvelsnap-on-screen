54039162b736abd547203304b5cf04392848d0ba
Z Yin <Z.Yin.MD@gmail.com>
2023-11-06 01:55:47 +0000

    dev container: re-organize postCreate and postStart script

---
+++ .devcontainer/postCreate.sh
@@ -0,0 +1,3 @@
+set -e
+
+cd ui && bun install
--- .devcontainer/postStart.sh
+++ .devcontainer/postStart.sh
@@ -1,6 +1,4 @@
 # install dependencies (node_modules) for ui:
 set -e
 
-cd ui
-bun install # this normally should exist in post-create, not post-start, but it only takes miliseconds, and creates one less interactive shell post-create, which is nice
-# bun dev
+# cd ui && bun dev # I no longer do this because it makes vscode treat the startup process as unfinished, show an everlasting spinner in the status bar, and also don't stop the "vsc-volume-bootstrap" container.
