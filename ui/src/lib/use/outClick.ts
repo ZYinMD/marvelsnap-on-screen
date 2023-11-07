69bc5bd48b0d691884efee6618c2dc8257784039
Z Yin <Z.Yin.MD@gmail.com>
2023-11-07 00:21:42 +0000

    ui: add "outClick" action

---
+++ ui/src/lib/use/outClick.ts
@@ -0,0 +1,21 @@
+import type { Action } from 'svelte/action';
+
+type OutClick = Action<HTMLElement, undefined, { 'on:outClick'?: (e: CustomEvent) => void }>;
+/**
+ * a svelte action to be used with "use:".
+ * The node will be passed into this function on mount.
+ * On mount, add click listener to the whole document, if any click is captured and is not inside this node, fire custom event "outClick".
+ */
+export const outClick: OutClick = function (node) {
+  const handleClick = (event: MouseEvent) => {
+    if (!node.contains(event.target as Node)) {
+      node.dispatchEvent(new CustomEvent('outClick')); // both node node.dispatchEvent() and new CustomEvent() are native browser APIs. They can be used to crate an event with a custom name. The "createEventDispatcher" util from svelte actually uses this under the hood
+    }
+  };
+  document.addEventListener('click', handleClick, { capture: true });
+  return {
+    destroy() {
+      document.removeEventListener('click', handleClick, true);
+    },
+  };
+};
