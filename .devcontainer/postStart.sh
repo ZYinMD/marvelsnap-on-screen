62328db339fd3e1c93cf31082cc73d422478f8ce
Z Yin <Z.Yin.MD@gmail.com>
2023-11-08 21:01:00 +0000

    dev container: improve post start script

--- .devcontainer/postStart.sh
+++ .devcontainer/postStart.sh
@@ -2,4 +2,6 @@
 set -e
 
 
-# cd ui && bun dev # start the dev server. I no longer do this because it makes vscode treat the startup process as unfinished, show an everlasting spinner in the status bar, and also don't stop the "vsc-volume-bootstrap" container.
+echo -e "\e[34m\nReady. To start: cd ui && pnpm dev\e[0m"
+
+# Don't auto start a service here in this script, because it makes vscode treat the startup process as unfinished, show an everlasting spinner in the status bar, and also don't stop the "vsc-volume-bootstrap" container, may even not exit this container on disconnect and on rebuild'.
