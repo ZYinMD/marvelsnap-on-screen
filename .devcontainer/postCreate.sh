94f47b990b1c269edf94dcf5e6d7d8450ee4a250
Z Yin <Z.Yin.MD@gmail.com>
2023-11-08 00:09:47 +0000

    comments only

--- .devcontainer/postCreate.sh
+++ .devcontainer/postCreate.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -e
 
-# install dependencies (node_modules) for ui:
+# install dependencies (node_modules) for ui. This will also install the node version specified in .npmrc (the image itself doesn't have any node installed)
 cd ui && pnpm install
