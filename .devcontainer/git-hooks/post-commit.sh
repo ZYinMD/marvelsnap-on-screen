45262358347926581620908760f4e10345ff46cd
Z Yin <Z.Yin.MD@gmail.com>
2023-11-09 22:05:40 +0000

    dev container: set post commit hook to auto push

---
+++ .devcontainer/git-hooks/post-commit.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+# this script is run after every commit. See more in postCreate.sh
+
+# auto push up to HEAD~1, so that the remote branch is always behind by 1 commit. See https://stackoverflow.com/questions/77455698/is-it-possible-to-git-auto-push-after-commit-but-only-up-to-head2-so-that-rem.
+git push origin HEAD~1:"$(git branch --show-current)"
