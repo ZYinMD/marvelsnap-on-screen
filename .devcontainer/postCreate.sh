2ecd737ac5b722fdbcff044d2c673bf222ac2c0e
Z Yin <Z.Yin.MD@gmail.com>
2023-11-09 22:29:49 +0000

    add Executable Permission to post commit hook

--- .devcontainer/postCreate.sh
+++ .devcontainer/postCreate.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 set -e
 
+chmod +x .devcontainer/git-hooks/post-commit
+
 git config core.hooksPath "./.devcontainer/git-hooks"
+
 
 # install dependencies (node_modules) for ui. This will also install the node version specified in .npmrc (the image itself doesn't have any node installed)
 cd ui && pnpm install
