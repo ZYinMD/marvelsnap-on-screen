9563fe407e6422416c788ee11424bd45d55c173a
Z Yin <Z.Yin.MD@gmail.com>
2023-11-09 22:12:35 +0000

    fix git hook relative path again

--- .devcontainer/postCreate.sh
+++ .devcontainer/postCreate.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 
-git config core.hooksPath ".devcontainer/git-hooks"
+git config core.hooksPath "./.devcontainer/git-hooks"
 
 # install dependencies (node_modules) for ui. This will also install the node version specified in .npmrc (the image itself doesn't have any node installed)
 cd ui && pnpm install
