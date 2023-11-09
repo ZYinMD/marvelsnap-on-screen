b74c7002d4357f6e8a3b6b0a48d9765d8aba33db
Z Yin <Z.Yin.MD@gmail.com>
2023-11-09 22:33:02 +0000

    post create script: add comments

--- .devcontainer/postCreate.sh
+++ .devcontainer/postCreate.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 set -e
 
-chmod +x .devcontainer/git-hooks/post-commit
-
+# change the default git hook path to our own hooks folder in the repo
 git config core.hooksPath "./.devcontainer/git-hooks"
-
+chmod +x .devcontainer/git-hooks/post-commit # make our post-commit hook executable
 
 # install dependencies (node_modules) for ui. This will also install the node version specified in .npmrc (the image itself doesn't have any node installed)
 cd ui && pnpm install
