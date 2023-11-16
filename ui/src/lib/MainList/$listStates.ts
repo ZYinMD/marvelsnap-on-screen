4db87e0986b9f72f760e9996d6c5777d150f6906
Z Yin <Z.Yin.MD@gmail.com>
2023-11-16 04:34:11 +0000

    ui: add sort by year

--- ui/src/lib/MainList/$listStates.ts
+++ ui/src/lib/MainList/$listStates.ts
@@ -1,6 +1,7 @@
 import { derived, writable } from 'svelte/store';
 import { allTitles } from '../facts/allTitles';
 import type { Key } from '../facts/map';
+import { sortStates } from '../Sort/$sortStates';
 
 const dividerMovies = {
   title: 'Movies',
@@ -17,20 +18,36 @@ const dividerAnimatedTvSeries = {
   type: 'divider',
   key: 'heading-animated-tv-series',
 } as const;
+
+const movies = Object.values(allTitles.movie);
+const liveActionTvSeries = Object.values(allTitles['live-action-tv-series']);
+const animatedTvSeries = Object.values(allTitles['animated-tv-series']);
+const all = [...movies, ...liveActionTvSeries, ...animatedTvSeries];
+
+const compareYear = (descending: boolean) => (a: { year: string }, b: { year: string }) => {
+  let result = a.year > b.year;
+  if (descending) result = !result;
+  return result ? 1 : -1;
+};
 /**
  * The main list containing the data used to render the list on the home page. The list contains a mixture of 3 possible "dividers" objects and all the shows. The divider will be rendered as divider components, shows rendered as show components.
  * It's a derived store, will change its values based on the current sort and filter.
  */
-export const mainList = derived([], () => {
+export const mainList = derived(sortStates, (sortStates) => {
+  const { sortDescending: direction, sortBy, groupByType } = sortStates;
   const result = [];
-  result.push(dividerMovies);
-  result.push(...Object.values(allTitles.movie));
-
-  result.push(dividerLiveActionTvSeries);
-  result.push(...Object.values(allTitles['live-action-tv-series']));
-
-  result.push(dividerAnimatedTvSeries);
-  result.push(...Object.values(allTitles['animated-tv-series']));
+  {
+    result.push(dividerMovies);
+    result.push(...movies.sort(compareYear(direction)));
+  }
+  {
+    result.push(dividerLiveActionTvSeries);
+    result.push(...liveActionTvSeries.sort(compareYear(direction)));
+  }
+  {
+    result.push(dividerAnimatedTvSeries);
+    result.push(...animatedTvSeries.sort(compareYear(direction)));
+  }
   return result;
 });
 
