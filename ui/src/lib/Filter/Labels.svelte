af8cb91bfedd31063c653ec13e869f4655406185
Z Yin <Z.Yin.MD@gmail.com>
2023-11-20 22:38:47 +0000

    ui > filter: css only

--- ui/src/lib/Filter/Labels.svelte
+++ ui/src/lib/Filter/Labels.svelte
@@ -5,29 +5,25 @@
 <!-- @component Common labels used for filtering, e.g. MCU, Netflix, Anime, etc -->
 <ul class="container">
   <li>
-    <div class="label">Marvel Cinematic Universe</div>
-    <div><Info /></div>
+    <div class="label skew">Marvel Cinematic Universe</div>
+    <div class="info-button"><Info /></div>
   </li>
   <li>
-    <div class="label">20th Century Fox X-Men</div>
-    i
+    <div class="label skew">20th Century Fox X-Men</div>
+    <div class="info-button"><Info /></div>
   </li>
   <li>
-    <div class="label">Sony's Spider-Man Universe</div>
-    i
+    <div class="label skew">Sony's Spider-Man Universe</div>
+    <div class="info-button"><Info /></div>
   </li>
   <li>
-    <div class="label">Netflix</div>
-    i
+    <div class="label skew">Netflix</div>
+    <div class="info-button"><Info /></div>
   </li>
   <li>
-    <div class="label">Other continuities</div>
-    i
+    <div class="label skew">Other continuities</div>
+    <div class="info-button"><Info /></div>
   </li>
-  <li>1</li>
-  <li>1</li>
-  <li>1</li>
-  <li>1</li>
 </ul>
 
 <style>
@@ -38,5 +34,20 @@
   }
   li {
     display: flex;
+    align-items: center;
+  }
+  .label {
+    border: 1px solid grey;
+    padding: 4px 6px;
+    margin: 5px 0px;
+  }
+  .info-button {
+    font-size: 16px;
+    margin: 4.5px 0px 0px 10px;
+    color: #555;
+    scale: 0.9;
+  }
+  .skew {
+    transform: skew(-6deg);
   }
 </style>
