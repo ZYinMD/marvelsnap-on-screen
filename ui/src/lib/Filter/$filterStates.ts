703a28d1b9d29ebeb59ec15f60f9f57e4f6ebbf4
Z Yin <Z.Yin.MD@gmail.com>
2023-11-21 14:46:39 -0500

    ui > filter: now able to switch labels

--- ui/src/lib/Filter/$filterStates.ts
+++ ui/src/lib/Filter/$filterStates.ts
@@ -3,4 +3,16 @@ import { writable } from 'svelte/store';
 export const filterStates = writable({
   isPanelOpen: false,
   searching: '',
+  activeLabel: '' as
+    | ''
+    | 'mcu'
+    | 'x-men'
+    | 'ssu'
+    | 'netflix'
+    | 'sequel'
+    | 'all-tv'
+    | 'all-animated'
+    | 'anime',
 });
+
+filterStates.subscribe((state) => console.log(state.activeLabel));
--- ui/src/lib/Filter/Labels.svelte
+++ ui/src/lib/Filter/Labels.svelte
@@ -1,27 +1,85 @@
 <script lang="ts">
   import Info from '../Icons/Info.svelte';
+  import { filterStates } from './$filterStates';
+  const toggle = (label: typeof $filterStates.activeLabel) => () => {
+    if ($filterStates.activeLabel === label) {
+      $filterStates.activeLabel = '';
+    } else $filterStates.activeLabel = label;
+  };
 </script>
 
 <!-- @component Common labels used for filtering, e.g. MCU, Netflix, Anime, etc -->
 <ul class="container">
   <li>
-    <div class="label"><div class="skew">Marvel Cinematic Universe</div></div>
+    <div class="label" class:active={$filterStates.activeLabel === 'mcu'} on:click={toggle('mcu')}>
+      <div class="skew">Marvel Cinematic Universe</div>
+    </div>
     <div class="info-button"><Info /></div>
   </li>
   <li>
-    <div class="label"><div class="skew">20th Century Fox X-Men</div></div>
+    <div
+      class="label"
+      class:active={$filterStates.activeLabel === 'x-men'}
+      on:click={toggle('x-men')}
+    >
+      <div class="skew">20th Century Fox X-Men</div>
+    </div>
     <div class="info-button"><Info /></div>
   </li>
   <li>
-    <div class="label"><div class="skew">Sony's Spider-Man Universe</div></div>
+    <div class="label" class:active={$filterStates.activeLabel === 'ssu'} on:click={toggle('ssu')}>
+      <div class="skew">Sony's Spider-Man Universe</div>
+    </div>
     <div class="info-button"><Info /></div>
   </li>
   <li>
-    <div class="label"><div class="skew">Netflix</div></div>
+    <div
+      class="label"
+      class:active={$filterStates.activeLabel === 'netflix'}
+      on:click={toggle('netflix')}
+    >
+      <div class="skew">Netflix</div>
+    </div>
     <div class="info-button"><Info /></div>
   </li>
   <li>
-    <div class="label"><div class="skew">Other continuities</div></div>
+    <div
+      class="label"
+      class:active={$filterStates.activeLabel === 'sequel'}
+      on:click={toggle('sequel')}
+    >
+      <div class="skew">Other continuities</div>
+    </div>
+    <div class="info-button"><Info /></div>
+  </li>
+  <li>
+    <div
+      class="label"
+      class:active={$filterStates.activeLabel === 'all-tv'}
+      on:click={toggle('all-tv')}
+    >
+      <div class="skew">All TV Series</div>
+    </div>
+    <div class="info-button"><Info /></div>
+  </li>
+  <li>
+    <div
+      class="label"
+      class:active={$filterStates.activeLabel === 'all-animated'}
+      on:click={toggle('all-animated')}
+    >
+      <div class="skew">All Animated Series</div>
+    </div>
+    <div class="info-button"><Info /></div>
+  </li>
+  <li>
+    <div
+      class="label"
+      class:active={$filterStates.activeLabel === 'anime'}
+      on:click={toggle('anime')}
+    >
+      <div class="skew">Anime</div>
+    </div>
     <div class="info-button"><Info /></div>
   </li>
 </ul>
@@ -40,6 +98,11 @@
     border: 1px solid #555;
     padding: 4.5px 7px 4px;
     margin: 5px 0px;
+  }
+  .label.active {
+    border-color: skyblue;
+    background-color: skyblue;
+    color: white;
   }
   .info-button {
     font-size: 16px;
