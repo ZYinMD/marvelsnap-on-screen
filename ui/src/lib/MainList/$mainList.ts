5581fbd769a4cd6e89645b8eef0512cf831dcc07
Z Yin <Z.Yin.MD@gmail.com>
2023-11-21 17:10:12 -0500

    ui > mainList: refactor: extract functions

--- ui/src/lib/MainList/$mainList.ts
+++ ui/src/lib/MainList/$mainList.ts
@@ -1,7 +1,8 @@
 import { derived, writable } from 'svelte/store';
 import { sortStates } from '../Sort/$sortStates';
 import { allTitles } from '../facts/allTitles';
-import { map, type Key } from '../facts/map';
+import type { Key } from '../facts/map';
+import { sortFn } from './sortFn';
 
 const dividerMovies = {
   title: 'Movies',
@@ -22,58 +23,9 @@ const dividerAnimatedTvSeries = {
 const movies = Object.values(allTitles.movie);
 const liveActionTvSeries = Object.values(allTitles['live-action-tv-series']);
 const animatedTvSeries = Object.values(allTitles['animated-tv-series']);
-const all = [...movies, ...liveActionTvSeries, ...animatedTvSeries];
+const allShows = [...movies, ...liveActionTvSeries, ...animatedTvSeries];
 
-export type Show = (typeof all)[number];
-
-const compareYear = (descending: boolean) => (a: Show, b: Show) => {
-  let verdict = a.year > b.year;
-  if (descending) verdict = !verdict;
-  return verdict ? 1 : -1;
-};
-function getNumCardsByShow(show: Show) {
-  const roster = map[show.key];
-  return roster.major.size + roster.minor.size;
-}
-const compareNumCards = (descending: boolean) => (a: Show, b: Show) => {
-  let verdict = getNumCardsByShow(a) > getNumCardsByShow(b);
-  if (descending) verdict = !verdict;
-  return verdict ? 1 : -1;
-};
-const compareNumSeasons = (descending: boolean) => (a: Show, b: Show) => {
-  // @ts-expect-error: I know what I'm doing
-  const numSeasonsA = a.numSeasons || 0;
-  // @ts-expect-error: I know what I'm doing
-  const numEpisodesA = a.numEpisodes || 0;
-  // @ts-expect-error: I know what I'm doing
-  const numSeasonsB = b.numSeasons || 0;
-  // @ts-expect-error: I know what I'm doing
-  const numEpisodesB = b.numEpisodes || 0;
-  let verdict: boolean;
-  if (numSeasonsA === numSeasonsB) {
-    verdict = numEpisodesA > numEpisodesB;
-  } else verdict = numSeasonsA > numSeasonsB;
-  if (descending) verdict = !verdict;
-  return verdict ? 1 : -1;
-};
-const compareTitle = (descending: boolean) => (a: Show, b: Show) => {
-  let titleA = a.title.toLowerCase();
-  let titleB = b.title.toLowerCase();
-  if (titleA.startsWith('the ')) titleA = titleA.slice(4);
-  if (titleB.startsWith('the ')) titleB = titleB.slice(4);
-  let verdict = titleA > titleB;
-  if (descending) verdict = !verdict;
-  return verdict ? 1 : -1;
-};
-
-export const sortFn = derived(sortStates, (sortStates) => {
-  const { sortDescending: direction, sortBy } = sortStates;
-  if (sortBy === 'year') return compareYear(direction);
-  else if (sortBy === 'numCards') return compareNumCards(direction);
-  else if (sortBy === 'numSeasons') return compareNumSeasons(direction);
-  else if (sortBy === 'alphabetical') return compareTitle(direction);
-  else throw 'unknown sortBy value';
-});
+export type Show = (typeof allShows)[number];
 
 /**
  * The main list containing the data used to render the list on the home page. The list may contains a mixture of "dividers" objects and shows. The divider will be rendered as divider components, displaying some text. The shows are rendered as show components.
---
+++ ui/src/lib/MainList/sortFn.ts
@@ -0,0 +1,57 @@
+import { derived } from 'svelte/store';
+import { sortStates } from '../Sort/$sortStates';
+import type { Show } from './$mainList';
+import { map } from '../facts/map';
+
+export const compareYear = (descending: boolean) => (a: Show, b: Show) => {
+  let verdict = a.year > b.year;
+  if (descending) verdict = !verdict;
+  return verdict ? 1 : -1;
+};
+function getNumCardsByShow(show: Show) {
+  const roster = map[show.key];
+  return roster.major.size + roster.minor.size;
+}
+export const compareNumCards = (descending: boolean) => (a: Show, b: Show) => {
+  let verdict = getNumCardsByShow(a) > getNumCardsByShow(b);
+  if (descending) verdict = !verdict;
+  return verdict ? 1 : -1;
+};
+export const compareNumSeasons = (descending: boolean) => (a: Show, b: Show) => {
+  // @ts-expect-error: I know what I'm doing
+  const numSeasonsA = a.numSeasons || 0;
+  // @ts-expect-error: I know what I'm doing
+  const numEpisodesA = a.numEpisodes || 0;
+  // @ts-expect-error: I know what I'm doing
+  const numSeasonsB = b.numSeasons || 0;
+  // @ts-expect-error: I know what I'm doing
+  const numEpisodesB = b.numEpisodes || 0;
+  let verdict: boolean;
+  if (numSeasonsA === numSeasonsB) {
+    verdict = numEpisodesA > numEpisodesB;
+  } else verdict = numSeasonsA > numSeasonsB;
+  if (descending) verdict = !verdict;
+  return verdict ? 1 : -1;
+};
+
+export const compareTitle = (descending: boolean) => (a: Show, b: Show) => {
+  let titleA = a.title.toLowerCase();
+  let titleB = b.title.toLowerCase();
+  if (titleA.startsWith('the ')) titleA = titleA.slice(4);
+  if (titleB.startsWith('the ')) titleB = titleB.slice(4);
+  let verdict = titleA > titleB;
+  if (descending) verdict = !verdict;
+  return verdict ? 1 : -1;
+};
+
+/**
+ * A store that returns a function. The store reacts to the sorting options in the bottom left corner. The function is to be passed into array.prototype.sort()
+ */
+export const sortFn = derived(sortStates, (sortStates) => {
+  const { sortDescending: direction, sortBy } = sortStates;
+  if (sortBy === 'year') return compareYear(direction);
+  else if (sortBy === 'numCards') return compareNumCards(direction);
+  else if (sortBy === 'numSeasons') return compareNumSeasons(direction);
+  else if (sortBy === 'alphabetical') return compareTitle(direction);
+  else throw 'unknown sortBy value';
+});
