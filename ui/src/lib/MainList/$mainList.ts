689776116a9caab37db4162177a92fe1799a7865
Z Yin <Z.Yin.MD@gmail.com>
2023-12-09 00:27:08 -0500

    ui: refactor f2 folder name only

--- ui/src/lib/MainList/$mainList.ts
+++ ui/src/lib/MainList/$mainList.ts
@@ -1,8 +1,8 @@
 import { derived } from 'svelte/store';
-import { animatedTv } from '../stores/derived/showLists/$animated-tv';
-import { liveActionTv } from '../stores/derived/showLists/$live-action-tv';
-import { mixed } from '../stores/derived/showLists/$mixed';
-import { movies } from '../stores/derived/showLists/$movies';
+import { animatedTv } from '../stores/derived/subLists/$animated-tv';
+import { liveActionTv } from '../stores/derived/subLists/$live-action-tv';
+import { mixed } from '../stores/derived/subLists/$mixed';
+import { movies } from '../stores/derived/subLists/$movies';
 
 /**
  * The main list containing the data used to render the list on the home page. The list may contains a mixture of "dividers" objects and shows. The divider will be rendered as divider components, displaying some text. The shows are rendered as show components.
