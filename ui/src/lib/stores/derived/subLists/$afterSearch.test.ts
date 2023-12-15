ff0fa7a27d656f57ce531079ee15b6834d8ecd0e
Z Yin <Z.Yin.MD@gmail.com>
2023-12-15 17:14:05 -0500

    ui: refactor: rewrite unit test in test.each

--- ui/src/lib/stores/derived/subLists/$afterSearch.test.ts
+++ ui/src/lib/stores/derived/subLists/$afterSearch.test.ts
@@ -1,50 +1,64 @@
-import { expect, test } from 'vitest';
+import { describe, expect, test } from 'vitest';
 import { allTitles } from '../../../facts/allTitles';
 import { matchAll } from './$afterSearch';
 
-test('match one show', () => {
-  {
-    const show = allTitles.movie['2005|Fantastic Four'];
-    expect(matchAll(show, '2')).toBe(false);
-    expect(matchAll(show, '200')).toBe(true);
-    expect(matchAll(show, '2005')).toBe(true);
-    expect(matchAll(show, '2006')).toBe(false);
-    expect(matchAll(show, 'f')).toBe(false);
-    expect(matchAll(show, 'f f')).toBe(false);
-    expect(matchAll(show, 'fantastic')).toBe(true);
-    expect(matchAll(show, 'fantastic four')).toBe(true);
-    expect(matchAll(show, 'four thing')).toBe(true);
-    expect(matchAll(show, 'five')).toBe(false);
-    expect(matchAll(show, 'doom')).toBe(true);
-    expect(matchAll(show, 'boom')).toBe(false);
-    expect(matchAll(show, 'inv woman')).toBe(true);
-    expect(matchAll(show, 'inv women')).toBe(false);
-  }
-  {
-    const show = allTitles['live-action-tv-series']['2021|WandaVision'];
-    expect(matchAll(show, '0')).toBe(false);
-    expect(matchAll(show, '200')).toBe(false);
-    expect(matchAll(show, '201')).toBe(false);
-    expect(matchAll(show, '202')).toBe(true);
-    expect(matchAll(show, '2021')).toBe(true);
-    expect(matchAll(show, 'wanda')).toBe(true);
-    expect(matchAll(show, 'vision')).toBe(true);
-    expect(matchAll(show, 'w v')).toBe(false);
-    expect(matchAll(show, 'scarlet')).toBe(true);
-    expect(matchAll(show, 'scar wanda')).toBe(true);
-    expect(matchAll(show, 'scar spectrum')).toBe(true);
-    expect(matchAll(show, 'widow')).toBe(false);
-    expect(matchAll(show, 'scarlet spectrum')).toBe(true); // if user searches for two cards, should match. This design actually leads to undesirable cases e.g. if user search "black wi" and if a movie has both "black bolt" and "scarlet witch", it will match, but that show doesn't have black widow which is probably what the user is looking for. However, I think it's fine.
-  }
-  {
-    const show = allTitles['animated-tv-series']['1994–1998|Spider-Man: The Animated Series'];
-    expect(matchAll(show, '199')).toBe(true);
-    expect(matchAll(show, '1998')).toBe(true);
-    expect(matchAll(show, '1997')).toBe(false);
-    expect(matchAll(show, 'spider')).toBe(true);
-    expect(matchAll(show, 'spider man')).toBe(true);
-    expect(matchAll(show, 'goblin')).toBe(true);
-    expect(matchAll(show, 'goblin animated')).toBe(true);
-    expect(matchAll(show, 'thor')).toBe(false);
-  }
+describe('test a movie', () => {
+  const movie = allTitles.movie['2005|Fantastic Four'];
+  test.each([
+    ['2', false],
+    ['200', true],
+    ['2', false],
+    ['200', true],
+    ['2005', true],
+    ['2006', false],
+    ['f', false],
+    ['f f', false],
+    ['fantastic', true],
+    ['fantastic four', true],
+    ['four thing', true],
+    ['five', false],
+    ['doom', true],
+    ['boom', false],
+    ['inv woman', true],
+    ['inv women', false],
+  ])(`"2005|Fantastic Four" matches %s: %b)`, (searchString, expected) => {
+    expect(matchAll(movie, searchString, true)).toBe(expected);
+  });
+});
+
+describe('test a tv show', () => {
+  const show = allTitles['live-action-tv-series']['2021|WandaVision'];
+  test.each([
+    ['0', false],
+    ['200', false],
+    ['201', false],
+    ['202', true],
+    ['2021', true],
+    ['wanda', true],
+    ['vision', true],
+    ['w v', false],
+    ['scarlet', true],
+    ['scar wanda', true],
+    ['scar spectrum', true],
+    ['widow', false],
+    ['scarlet spectrum', true], // if user searches for two cards, should match. This design actually leads to undesirable cases e.g. if user search "black wi" and if a movie has both "black bolt" and "scarlet witch", it will match, but that show doesn't have black widow which is probably what the user is looking for. However, I think it's fine.
+  ])(`"2021|WandaVision" matches %s: %b)`, (searchString, expected) => {
+    expect(matchAll(show, searchString, true)).toBe(expected);
+  });
+});
+
+describe('test a tv show', () => {
+  const show = allTitles['animated-tv-series']['1994–1998|Spider-Man: The Animated Series'];
+  test.each([
+    ['199', true],
+    ['1998', true],
+    ['1997', false],
+    ['spider', true],
+    ['spider man', true],
+    ['goblin', true],
+    ['goblin animated', true],
+    ['thor', false],
+  ])(`"1994–1998|Spider-Man: The Animated Series" matches %s: %b)`, (searchString, expected) => {
+    expect(matchAll(show, searchString, true)).toBe(expected);
+  });
 });
