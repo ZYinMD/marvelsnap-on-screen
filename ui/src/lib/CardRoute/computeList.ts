96f173ab2878f2f3bb67b0cd1005696d50ecc1c7
Z Yin <Z.Yin.MD@gmail.com>
2023-12-19 23:59:55 -0500

    ui: improve wording

--- ui/src/lib/CardRoute/computeList.ts
+++ ui/src/lib/CardRoute/computeList.ts
@@ -15,7 +15,7 @@ export function computeList(cardId: CardId) {
     return map[i.key].major.has(cardName) || map[i.key].minor.has(cardName);
   });
   if (movies.length > 0) {
-    result.push({ type: 'divider', title: 'Movies', key: 'movies' });
+    result.push({ type: 'divider', title: 'Appeared in movies', key: 'movies' });
     result.push(...movies);
   }
   const liveActionTVs = allLiveActionTVs.filter((i) => {
@@ -24,7 +24,7 @@ export function computeList(cardId: CardId) {
   if (liveActionTVs.length > 0) {
     result.push({
       type: 'divider',
-      title: 'Live Action TV series',
+      title: 'In live Action TV series',
       key: 'Live Action TV series',
     });
     result.push(...liveActionTVs);
@@ -33,7 +33,7 @@ export function computeList(cardId: CardId) {
     return map[i.key].major.has(cardName) || map[i.key].minor.has(cardName);
   });
   if (animatedTVs.length > 0) {
-    result.push({ type: 'divider', title: 'Animated TV series', key: 'Animated TV series' });
+    result.push({ type: 'divider', title: 'In animated TV series', key: 'Animated TV series' });
     result.push(...animatedTVs);
   }
   return result;
