a4b67102d42c01bdc11ce2e539960dd2e11c980b
Z Yin <Z.Yin.MD@gmail.com>
2023-12-21 12:15:23 -0500

    ui: simplify css for background

--- ui/src/lib/Background/Background.svelte
+++ ui/src/lib/Background/Background.svelte
@@ -1,22 +1,11 @@
 <script lang="ts">
-  import { dimBackground } from '../stores/derived/$dimBackground';
   import Hexagons from './Hexagons.svelte';
   import SlidingDiagonals from './SlidingDiagonals.svelte';
 </script>
 
 <!-- @component The animated background of app -->
-<div class="component container-all-viewport" class:dim={$dimBackground}>
-  <!-- <RotatingMix /> -->
-  <Hexagons />
-  <SlidingDiagonals />
-</div>
+<Hexagons />
+<SlidingDiagonals />
 
 <style>
-  .component {
-    display: grid;
-    grid-template-areas: 'the-only';
-  }
-  .dim {
-    filter: brightness(70%);
-  }
 </style>
