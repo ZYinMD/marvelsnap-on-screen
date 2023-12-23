7f41cf9c1515b77dced15b8402a9a6e37334f5b2
Z Yin <Z.Yin.MD@gmail.com>
2023-12-23 12:45:59 -0500

    ui: dim background when big card showing

--- ui/src/lib/stores/derived/$dimBackground.ts
+++ ui/src/lib/stores/derived/$dimBackground.ts
@@ -1,8 +1,19 @@
 import { derived } from 'svelte/store';
 import { filter, sort, tooltip } from '../writables/$primary';
+import { page } from '$app/stores';
 
-export const dimBackground = derived([sort, filter, tooltip], ([sort, filter, tooltip]) => {
-  const aPanelIsOpen = sort.isPanelOpen || filter.isPanelOpen;
-  const aTooltipIsOpen = Boolean(tooltip);
-  return aPanelIsOpen || aTooltipIsOpen;
-});
+/**
+ * should most of the view be dimmed because something is showing in the foreground?
+ */
+export const dimBackground = derived(
+  [sort, filter, tooltip, page],
+  ([sort, filter, tooltip, page]) => {
+    const aBigCardIsShowing = Boolean(page.state.cardId);
+    if (aBigCardIsShowing) return true;
+    const aPanelIsOpen = sort.isPanelOpen || filter.isPanelOpen;
+    if (aPanelIsOpen) return true;
+    const aTooltipIsOpen = Boolean(tooltip);
+    if (aTooltipIsOpen) return true;
+    return false;
+  },
+);
