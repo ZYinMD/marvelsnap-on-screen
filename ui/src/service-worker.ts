fff1ffbe7655f20bdd513919bafa1df7ef435259
Z Yin <Z.Yin.MD@gmail.com>
2024-01-05 00:14:42 -0500

    add clientsClaim and skipWaiting

--- ui/src/service-worker.ts
+++ ui/src/service-worker.ts
@@ -1,6 +1,12 @@
 /// <reference types="@sveltejs/kit" />
+/// <reference lib="webworker" />
+
+import { clientsClaim } from 'workbox-core';
 import { googleFontsCache, imageCache } from 'workbox-recipes';
 
-googleFontsCache();
+declare const self: ServiceWorkerGlobalScope;
 
+googleFontsCache();
 imageCache({ maxEntries: 300 });
+clientsClaim(); // take control on the 1st visit, so images can start being cached
+self.skipWaiting(); // activate new service worker immediately after install. This is not good if i have precache, but I don't have precache. (however, this is likely not actually useful for me either)
